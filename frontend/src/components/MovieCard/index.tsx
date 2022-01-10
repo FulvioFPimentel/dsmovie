369a91bb75c47c18ef9b0a41cdac67d6dfaa5eb1
FulvioFPimentel <fulviofp@hotmail.com>
2022-01-10 17:31:42 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,5 @@
 import MovieScore from "../MovieScore";
+import { Link } from 'react-router-dom';
 
 const MovieCard = () => {
 
@@ -12,13 +13,17 @@ const MovieCard = () => {
 
     return (
         <div>
-    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-    <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <MovieScore />
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-    </div>
-</div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <MovieScore />
+
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
+            </div>
+        </div>
     )
 }
 
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
-import './styles.css'
+import { Link } from 'react-router-dom';
+import './styles.css';
 
 const Form = () => {
     const movie = {
@@ -11,31 +12,34 @@ const Form = () => {
 
     return (
         <div className="dsmovie-form-container">
-        <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-        <div className="dsmovie-card-bottom-container">
-            <h3>{movie.title}</h3>
-            <form className="dsmovie-form">
-                <div className="form-group dsmovie-form-group">
-                    <label htmlFor="email">Informe seu email</label>
-                    <input type="email" className="form-control" id="email" />
-                </div>
-                <div className="form-group dsmovie-form-group">
-                    <label htmlFor="score">Informe sua avaliação</label>
-                    <select className="form-control" id="score">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-                <div className="dsmovie-form-btn-container">
-                    <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
-                </div>
-            </form >
-            <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="dsmovie-form">
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                    </div>
+                </form >
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
+            </div >
         </div >
-    </div >
     )
 }
 
