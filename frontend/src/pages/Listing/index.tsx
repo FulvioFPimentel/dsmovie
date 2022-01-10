dfc84a5e9e84731d2f42cb19524b344a983d10e9
FulvioFPimentel <fulviofp@hotmail.com>
2022-01-10 14:34:27 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,9 @@
+import Pagination from "../../components/Pagination";
+import './styles.css'
 
 const Listing = () => {
     return(
-        <h1>Página de listagem</h1>
+        <Pagination />
     );
 }
 
