9e1b908fe7061ca81b384af7703f08f79d92c96f
FulvioFPimentel <fulviofp@hotmail.com>
2022-01-12 23:23:38 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsmovie.services;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
