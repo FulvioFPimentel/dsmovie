fec2d2f4b072068cb59c66a96479a9b2e03d53c6
FulvioFPimentel <fulviofp@hotmail.com>
2022-01-14 12:08:45 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,18 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 import './styles.css'
 
 const Listing = () => {
+
+    // FORMA ERRADA
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data)
+        })
+
     return (
         <>
             <Pagination />
---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,21 @@
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+
+export type Movie = {
+    id: number;
+    title: string;
+    score: number;
+    count: number;
+    image: string;
+}
+
+export type MoviePage = {
+    content: Movie[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+    first: boolean;
+    numberOfElements: number;
+    empty: boolean;
+}
